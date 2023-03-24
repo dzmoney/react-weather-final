c83e62a0ff556a976111af31b52bdafef176ee66
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-03-24 18:15:34 -0400

    created Search component for search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Search from "./Search";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <Search />
+        <Weather />
+        <footer>
+          Coded by Deanna Tyndall,{" "}
+          <a href="https://github.com/dzmoney/react-weather-final">
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Search.css";
+
+export default function Search() {
+  return (
+    <div className="Search">
+      <input type="search" placeholder="Enter a city.." />
+      <button type="button" class="btn btn-primary">
+        Search
+      </button>
+    </div>
+  );
+}
