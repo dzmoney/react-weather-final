6f8309c009a688e9bc286dd0b6be3120e44e1e41
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-03-24 23:38:24 -0400

    more tweaking CSS positioning

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ export default function Search() {
   return (
     <div className="Search">
       <form>
-        <input type="search" placeholder="Enter a city.." />
+        <input type="search" placeholder="Enter a city.." autoFocus="on" />
         <button type="button" className="btn btn-primary">
           Search
         </button>
